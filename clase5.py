ace064808574171f1edb5ee01f5e1338ea448272
isabelsanzp99-svg <isabelsanchezp.99@gmail.com>
2026-08-31 12:32:36 -0600

    Add files via upload

---
+++ clase5.py
@@ -0,0 +1,20 @@
+
+
+#If - else
+
+print("Llegas a una nueva ciudad-")
+
+viajar_1 = input("¿Deseas viajar a otra ciudad? (True/False): ")
+viajar_2 = input("Deseas viajar a la segunda ciudad? (True/False):")
+viajar_3 = input("Deseas viajar a la tercera ciudad? (True/False):")
+
+if viajar_1 == "True":
+    print("Continua a la siguiente ciudad 1.")
+elif viajar_2 == "True": 
+    print("Continua a la siguiente ciudad_2.")
+elif viajar_3 == "True":
+    print("Continua a la siguiente ciudad_3.")
+else: 
+    print("Gracias por tu visita, regresa pronto.")
+
+    
